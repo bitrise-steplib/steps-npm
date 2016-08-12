99b4bdf233b1f488b5ecb55583bffb916b3d3b35
RheeseyB <rheese.burgess@googlemail.com>
2016-08-12 10:03:35 +0100

    Added working directory input (#1)

--- step.sh
+++ step.sh
@@ -2,4 +2,13 @@
 
 set -e
 
+if [ ! -z "${workdir}" ] ; then
+  echo "==> Switching to working directory: ${workdir}"
+  cd "${workdir}"
+  if [ $? -ne 0 ] ; then
+    echo " [!] Failed to switch to working directory: ${workdir}"
+    exit 1
+  fi
+fi
+
 npm ${command} ${options}
