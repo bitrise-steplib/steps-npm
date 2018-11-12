e234013b7e54590b89b50a9b4a030e59bd546e34
Laszlo Szucs <laszlo.szucs@bitrise.io>
2018-11-12 14:36:28 +0100

    npm version detection

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"os/exec"
+	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/fileutil"
 )
 
@@ -12,10 +15,47 @@ type jsonModel struct {
 	}
 }
 
-func main() {
+type ConfigsModel struct {
+	Workdir    string
+	Command    string
+	NpmVersion string
+}
+
+func createConfigsModelFromEnvs() ConfigsModel {
+	return ConfigsModel{
+		Workdir:    os.Getenv("workdir"),
+		Command:    os.Getenv("command"),
+		NpmVersion: os.Getenv("npm_version"),
+	}
+}
+
+func getNpmVersionFromPackageJson(path string) (string) {
 	content, _ := fileutil.ReadStringFromFile("package.json")
 	var m jsonModel;
-
 	_ = json.Unmarshal([]byte(content), &m)
-	fmt.Printf("detected npm version: %s\n", m.Engines.Npm)
+	return m.Engines.Npm
+}
+
+func getNpmVersionFromSystem() string {
+	_, error := exec.LookPath("npm")
+
+	if (error != nil) {
+
+	}
+
+	out, _ := command.RunCmdAndReturnTrimmedOutput(command.New("npm", "--version").GetCmd())
+	return out
+}
+
+func main() {
+	
+	config := createConfigsModelFromEnvs()
+	if config.NpmVersion == "" {
+		config.NpmVersion = getNpmVersionFromPackageJson("package.json")
+		if config.NpmVersion == "" {
+			config.NpmVersion = getNpmVersionFromSystem()
+		}
+	}
+
+	fmt.Printf("detected npm version: %s\n", config.NpmVersion)
 }
