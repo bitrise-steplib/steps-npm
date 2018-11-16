5b0699afe594a2fe2f46dbf4e3227c9fe8ea799d
Laszlo Szucs <laszlo.szucs@bitrise.io>
2018-11-16 13:06:59 +0100

    run user specified npm command

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/errorutil"
 	"github.com/bitrise-io/go-utils/fileutil"
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-tools/go-steputils/stepconf"
@@ -19,7 +20,7 @@ type jsonModel struct {
 	}
 }
 
-type ConfigsModel struct {
+type Config struct {
 	Workdir    string `env:"workdir"`
 	Command    string `env:"command,required"`
 	NpmVersion string `env:"npm_version"`
@@ -66,8 +67,6 @@ func createInstallNpmCommand(platform string) *command.Model {
 }
 
 func installLatestNpm() error {
-	fmt.Printf("INFO: npm binary not found on PATH, installing latest")
-	
 	installNpmCmd := createInstallNpmCommand(runtime.GOOS)
 	if installNpmCmd == nil {
 		return errors.New("FATAL ERROR: not supported OS version")
@@ -81,7 +80,7 @@ func failf(f string, args ...interface{}) {
 	os.Exit(1)
 }
 
-func (configs ConfigsModel) print() {
+func (configs Config) print() {
 	fmt.Println()
 	log.Infof("Configs:")
 	log.Printf(" - Workdir: %s", configs.Workdir)
@@ -90,8 +89,14 @@ func (configs ConfigsModel) print() {
 	fmt.Println()
 }
 
+func setNpmVersion(ver string) {
+	// todo: run npm update to specific version
+	// todo: log command and output
+	runNpmCommand("npm run " + ver)
+}
+
 func main() {
-	var config ConfigsModel
+	var config Config
 	if err := stepconf.Parse(&config); err != nil {
 		failf("Couldn't create step config: %v\n", err)
 	}
@@ -103,6 +108,7 @@ func main() {
 		if ver := getNpmVersionFromPackageJson(); ver == "" {
 			log.Warnf("No npm version found in package.json! Falling back to installed npm.")
 			if _, err := exec.LookPath("npm"); err != nil {
+				log.Warnf("npm not found on PATH, installing latest version")
 				if err := installLatestNpm(); err != nil {
 					failf("Couldn't install npm: %v", err)
 				}
@@ -110,7 +116,24 @@ func main() {
 				ver = getNpmVersionFromSystem()
 			}
 
-			log.Donef("Step success")
+		} else {
+			setNpmVersion(ver)
+		}
+	} else {
+		setNpmVersion(config.NpmVersion)
+	}
+
+	cmd := command.New("npm", config.Command)
+	out, npmErr := cmd.RunAndReturnTrimmedCombinedOutput()
+	if npmErr != nil {
+		if errorutil.IsExitStatusError(npmErr) {
+			exitCode, err := errorutil.CmdExitCodeFromError(npmErr)
+
+			failf("npm exit code %s, %s", exitCode, npmErr)
+		} else {
+			failf("%s failed: %s", cmd.PrintableCommandArgs(), out)
 		}
 	}
+
+	log.Donef("Step success")
 }
